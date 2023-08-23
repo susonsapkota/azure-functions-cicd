75c5f2f81c1f4ef8a06c195970549f037a4378c5
Suson Sapkota <susonsapkota@gmail.com>
2023-08-23 16:21:00 +0545

    Update testFunc.cs

--- testFunc.cs
+++ testFunc.cs
@@ -26,7 +26,7 @@ namespace Company.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTTP triggered function executed successfully and beautifully.Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
